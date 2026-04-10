600f15cc3fe15ec179d64b79dc9ddbe98d9b4b1e
ANSDB Developer <developer@ansdb.org>
2026-04-10 19:27:05 +0530

    feat: add top-level domain redirect for ims.ansdb.org

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
+  useEffect(() => {
+    // Top-Level domain redirect for specific vanity ERP links
+    if (window.location.hostname === 'ims.ansdb.org' && window.location.pathname === '/') {
+      window.location.replace('https://ansdb.org/erp/login?portal=institute');
+    }
+  }, []);
+
   return <AppRoutes />;
 }
 
